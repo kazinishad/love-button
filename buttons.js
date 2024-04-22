405e385ba185e990e59188310e7f57b5c2e82736
kazinishad <meem.kazi.789@gmail.com>
2024-04-23 00:40:18 +0600

    added first file

---
+++ buttons.js
@@ -0,0 +1,27 @@
+function buttons(){
+    document.body.style.backgroundColor='white';
+    document.body.style.color='black';
+    const ami=document.getElementById("secret");
+    ami.classList.add('hidden');
+    const tmi=document.getElementById('home');
+    tmi.classList.remove('hidden');
+    const btnn=document.getElementById('btn');
+    btnn.classList.remove('hidden');
+    const titlee=document.getElementById('title');
+    titlee.classList.remove('hidden');
+}
+function buttonss(){
+    document.body.style.backgroundColor='crimson';
+    document.body.style.color='white';
+    const ami=document.getElementById("secret");
+    ami.classList.remove('hidden');
+    const tmi=document.getElementById('home');
+    tmi.classList.add('hidden');
+    const btnn=document.getElementById('btn');
+    btnn.classList.add('hidden');
+    const titlee=document.getElementById('title');
+    titlee.classList.add('hidden');
+}
+function secrets(){
+    
+}
