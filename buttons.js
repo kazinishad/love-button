fd2f80dc3ae574c23d4260a3dc2fbed31065e049
kazinishad <meem.kazi.789@gmail.com>
2024-04-24 00:42:39 +0600

    scnd pic added

--- buttons.js
+++ buttons.js
@@ -11,6 +11,12 @@ function buttons(){
     titlee.classList.remove('hidden');
     const img=document.getElementById('img');
     img.classList.add('hidden');
+    const latter=document.getElementById('latter');
+    latter.classList.add('hidden');
+    const img2=document.getElementById('img1');
+    img2.classList.add('hidden');
+    const latter2=document.getElementById('latter2');
+    latter2.classList.add('hidden');
 }
 function buttonss(){
     document.body.style.backgroundColor='crimson';
@@ -25,6 +31,12 @@ function buttonss(){
     titlee.classList.add('hidden');
     const img=document.getElementById('img');
     img.classList.remove('hidden');
+    const latter=document.getElementById('latter');
+    latter.classList.remove('hidden');
+    const img2=document.getElementById('img1');
+    img2.classList.remove('hidden');
+    const latter2=document.getElementById('latter2');
+    latter2.classList.remove('hidden');
 }
 function secrets(){
     
